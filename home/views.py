1771137b9fb471583e25851b272a4aaf638eedd7
nurlan-python-dev <pirjanovnurlan77@gmail.com>
2022-05-08 15:11:45 +0500

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,7 +3,6 @@ from django.views.generic.base import TemplateView
 from accounts.models import CustomUser
 from task.models import TaskManager
 class HomeListView(LoginRequiredMixin, TemplateView):
-
     template_name = 'home.html'
     def get_context_data(self, *args, **kwargs):
         context = super(HomeListView, self).get_context_data()
